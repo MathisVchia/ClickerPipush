44daefc13fd6354188e98fed1645013095851377
MathisVchia <118368064+MathisVchia@users.noreply.github.com>
2024-01-08 19:00:00 +0100

    Reglage Probleme Shop

--- Assets/upgradeManager.cs
+++ Assets/upgradeManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class upgradeManager : MonoBehaviour
 {
+
+    public shopManagement shopManagement;
+
     // Start is called before the first frame update
     void Start()
     {
