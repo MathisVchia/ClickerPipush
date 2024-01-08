bb6da734c3027d8b284cdf88ba085d08f036ab28
MathisVchia <118368064+MathisVchia@users.noreply.github.com>
2024-01-08 21:11:01 +0100

    J'en ai marre

--- Assets/upgradeManager.cs
+++ Assets/upgradeManager.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class upgradeManager : MonoBehaviour
+public class UpgradeManager : MonoBehaviour
 {
-
     public shopManagement shopManagement;
-    public int tickets;
+    public GameObject upgradeButton; // Faites glisser votre bouton UI dans cette variable dans l'inspecteur Unity
+    public bool upgradeButtons = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        upgradeButtons = false;
+        // Assurez-vous que le bouton est initialement invisible au démarrage
+        SetUpgradeButtonVisibility(false);
     }
 
     // Update is called once per frame
@@ -20,6 +21,26 @@ public class upgradeManager : MonoBehaviour
         if (shopManagement.bonus > 10)
         {
             upgradeButtons = true;
+            Debug.Log("Upgrade buttons are now available!");
+            SetUpgradeButtonVisibility(true); // Rendre le bouton visible
+        }
+        else
+        {
+            upgradeButtons = false;
+            SetUpgradeButtonVisibility(false); // Rendre le bouton invisible
+        }
+    }
+
+    // Méthode pour définir la visibilité du bouton
+    void SetUpgradeButtonVisibility(bool isVisible)
+    {
+        if (upgradeButton != null)
+        {
+            upgradeButton.SetActive(isVisible);
+        }
+        else
+        {
+            Debug.LogWarning("Veuillez faire glisser le bouton UI dans la variable Upgrade Button dans l'inspecteur Unity.");
         }
     }
 }
