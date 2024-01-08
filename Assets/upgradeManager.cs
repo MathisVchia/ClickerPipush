1509e3fc55efc831167e6c5f4cb2db6a4b2b89cd
MathisVchia <118368064+MathisVchia@users.noreply.github.com>
2024-01-08 20:07:13 +0100

    De la merde, encore de la merde, toujours de la merde

--- Assets/upgradeManager.cs
+++ Assets/upgradeManager.cs
@@ -6,16 +6,20 @@ public class upgradeManager : MonoBehaviour
 {
 
     public shopManagement shopManagement;
+    public int tickets;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        upgradeButtons = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shopManagement.bonus > 10)
+        {
+            upgradeButtons = true;
+        }
     }
 }
