549ef059da7e7060408c2b75aea6b1dca409625f
MathisVchia <118368064+MathisVchia@users.noreply.github.com>
2024-01-18 16:30:39 +0100

    new stickers

--- Assets/gatchaManagement.cs
+++ Assets/gatchaManagement.cs
@@ -9,8 +9,12 @@ public class gatchaManagement : MonoBehaviour
     public GameObject gatchaButton;
     public GameObject stickersCommon1;
     public GameObject stickersCommon2;
+    public GameObject stickersCommon3;
+    public GameObject stickersCommon4;
     public GameObject stickersCommon1Carnet;
     public GameObject stickersCommon2Carnet;
+    public GameObject stickersCommon3Carnet;
+    public GameObject stickersCommon4Carnet;
     public GameObject passButton;
     public GameObject carnet;
     public GameObject closeCarnetButton;
@@ -24,9 +28,13 @@ public class gatchaManagement : MonoBehaviour
     public int Price = 30;
     public int nbStickersCommon = 0;
     public int nbStickersCommon2 = 0;
+    public int nbStickersCommon3 = 0;
+    public int nbStickersCommon4 = 0;
 
-    public int MaxRandomNormal;
-    public int MaxRandomGold;
+    public int MaxRandomStickers1;
+    public int MaxRandomStickers2;
+    public int MaxRandomStickers3;
+    public int MaxRandomStickers4;
 
     public GameObject RedParticle;
 
@@ -36,8 +44,12 @@ public class gatchaManagement : MonoBehaviour
         SetGatchaButtonVisibility(false);
         SetStickerCommon1Visibility(false);
         SetStickerCommon2Visibility(false);
+        SetStickerCommon3Visibility(false);
+        SetStickerCommon4Visibility(false);
         SetStickerCommon1CarnetVisibility(false);
         SetStickerCommon2CarnetVisibility(false);
+        SetStickerCommon3CarnetVisibility(false);
+        SetStickerCommon4CarnetVisibility(false);
         SetValidationButtonVisibility(false);
         SetCarnetVisibility(false);
         SetButtonCloseCarnetVisibility(false);
@@ -72,7 +84,7 @@ public class gatchaManagement : MonoBehaviour
         RandomLoot = Random.Range(Minimum, Maximum + 1);
 
         // Gagner un chat commun
-        if (RandomLoot <= MaxRandomNormal)
+        if (RandomLoot <= MaxRandomStickers1)
         {
             //showStickers = true;
 
@@ -83,7 +95,7 @@ public class gatchaManagement : MonoBehaviour
                 var vfx = Instantiate(RedParticle,Camera.main.transform);
                 vfx.transform.localPosition = new Vector3(0, 1, 10);
                 shopManagement.bonus = (shopManagement.bonus - Price);
-                shopManagement.bonusUI.text = "Ticket : " + shopManagement.bonus;
+                shopManagement.bonusUI.text = "" + shopManagement.bonus;
                 nbStickersCommon++;
                 //showStickers = false;
                 SetValidationButtonVisibility(true);
@@ -92,18 +104,17 @@ public class gatchaManagement : MonoBehaviour
         }
 
         // Gagner un chat commun 2
-        if (RandomLoot > MaxRandomNormal && RandomLoot <= MaxRandomGold)
+        if (RandomLoot > MaxRandomStickers1 && RandomLoot <= MaxRandomStickers2)
         {
             //showStickers = true;
 
             {
-                //stickerCommon1 = true;
                 Debug.Log("Upgrade buttons are now available!");
                 SetStickerCommon2Visibility(true); // Rendre le stickers visible
                 var vfx = Instantiate(RedParticle, Camera.main.transform);
                 vfx.transform.localPosition = new Vector3(0, 1, 10);
                 shopManagement.bonus = (shopManagement.bonus - Price);
-                shopManagement.bonusUI.text = "Ticket : " + shopManagement.bonus;
+                shopManagement.bonusUI.text = "" + shopManagement.bonus;
                 nbStickersCommon2++;
                 //showStickers = false;
                 SetValidationButtonVisibility(true);
@@ -111,6 +122,43 @@ public class gatchaManagement : MonoBehaviour
             }
         }
 
+        // Gagner un chat commun 3
+        if (RandomLoot > MaxRandomStickers2 && RandomLoot <= MaxRandomStickers3)
+        {
+            //showStickers = true;
+
+            {
+                Debug.Log("Upgrade buttons are now available!");
+                SetStickerCommon3Visibility(true); // Rendre le stickers visible
+                var vfx = Instantiate(RedParticle, Camera.main.transform);
+                vfx.transform.localPosition = new Vector3(0, 1, 10);
+                shopManagement.bonus = (shopManagement.bonus - Price);
+                shopManagement.bonusUI.text = "" + shopManagement.bonus;
+                nbStickersCommon3++;
+                //showStickers = false;
+                SetValidationButtonVisibility(true);
+
+            }
+        }
+
+        // Gagner un chat commun 4
+        if (RandomLoot > MaxRandomStickers3 && RandomLoot <= MaxRandomStickers4)
+        {
+            //showStickers = true;
+
+            {
+                Debug.Log("Upgrade buttons are now available!");
+                SetStickerCommon4Visibility(true); // Rendre le stickers visible
+                var vfx = Instantiate(RedParticle, Camera.main.transform);
+                vfx.transform.localPosition = new Vector3(0, 1, 10);
+                shopManagement.bonus = (shopManagement.bonus - Price);
+                shopManagement.bonusUI.text = "" + shopManagement.bonus;
+                nbStickersCommon4++;
+                //showStickers = false;
+                SetValidationButtonVisibility(true);
+
+            }
+        }
     }
 
     public void passVisual()
@@ -119,6 +167,8 @@ public class gatchaManagement : MonoBehaviour
         //{
             SetStickerCommon1Visibility(false);
             SetStickerCommon2Visibility(false);
+            SetStickerCommon3Visibility(false);
+            SetStickerCommon4Visibility(false);
             SetValidationButtonVisibility(false);
         //}
         //else
@@ -136,7 +186,7 @@ public class gatchaManagement : MonoBehaviour
             SetStickerCommon1CarnetVisibility(true);
 
             // Position aléatoire
-            Vector3 randomPosition = new Vector3(310, 380, 0);
+            Vector3 randomPosition = new Vector3(380, 510, 0);
             stickersCommon1Carnet.transform.position = randomPosition;
 
             // Rotation aléatoire
@@ -153,7 +203,7 @@ public class gatchaManagement : MonoBehaviour
             SetStickerCommon2CarnetVisibility(true);
 
             // Position aléatoire
-            Vector3 randomPosition = new Vector3(350, 460, 0);
+            Vector3 randomPosition = new Vector3(160, 380, 0);
             stickersCommon2Carnet.transform.position = randomPosition;
 
             // Rotation aléatoire
@@ -161,6 +211,40 @@ public class gatchaManagement : MonoBehaviour
             float randomRotationY = Random.Range(0f, 45f); // Modifiez les valeurs selon vos besoins
             Quaternion randomRotation = Quaternion.Euler(0, randomRotationY, 0);
             stickersCommon2Carnet.transform.rotation = randomRotation;
+
+            Debug.Log("Already have stickers");
+        }
+
+        if (nbStickersCommon3 >= 1)
+        {
+            SetStickerCommon3CarnetVisibility(true);
+
+            // Position aléatoire
+            Vector3 randomPosition = new Vector3(220, 610, 0);
+            stickersCommon3Carnet.transform.position = randomPosition;
+
+            // Rotation aléatoire
+            //float randomRotationX = Random.Range(0f, 45f); // Modifiez les valeurs selon vos besoins
+            float randomRotationY = Random.Range(0f, 45f); // Modifiez les valeurs selon vos besoins
+            Quaternion randomRotation = Quaternion.Euler(0, randomRotationY, 0);
+            stickersCommon3Carnet.transform.rotation = randomRotation;
+
+            Debug.Log("Already have stickers");
+        }
+
+        if (nbStickersCommon4 >= 1)
+        {
+            SetStickerCommon4CarnetVisibility(true);
+
+            // Position aléatoire
+            Vector3 randomPosition = new Vector3(250, 530, 0);
+            stickersCommon4Carnet.transform.position = randomPosition;
+
+            // Rotation aléatoire
+            //float randomRotationX = Random.Range(0f, 45f); // Modifiez les valeurs selon vos besoins
+            float randomRotationY = Random.Range(0f, 45f); // Modifiez les valeurs selon vos besoins
+            Quaternion randomRotation = Quaternion.Euler(0, randomRotationY, 0);
+            stickersCommon4Carnet.transform.rotation = randomRotation;
 
             Debug.Log("Already have stickers");
         }
@@ -173,6 +257,8 @@ public class gatchaManagement : MonoBehaviour
         SetButtonCloseCarnetVisibility(false);
         SetStickerCommon1CarnetVisibility(false);
         SetStickerCommon2CarnetVisibility(false);
+        SetStickerCommon3CarnetVisibility(false);
+        SetStickerCommon4CarnetVisibility(false);
     }
 
     // Méthode pour définir la visibilité du bouton
@@ -211,6 +297,30 @@ public class gatchaManagement : MonoBehaviour
         }
     }
 
+    // Méthode pour définir la visibilité du bouton
+    void SetStickerCommon3Visibility(bool isVisible)
+    {
+        if (stickersCommon3 != null)
+        {
+            stickersCommon3.SetActive(isVisible);
+        }
+        else
+        {
+        }
+    }
+
+    // Méthode pour définir la visibilité du bouton
+    void SetStickerCommon4Visibility(bool isVisible)
+    {
+        if (stickersCommon4 != null)
+        {
+            stickersCommon4.SetActive(isVisible);
+        }
+        else
+        {
+        }
+    }
+
 
 
     // STICKERS DU CARNET
@@ -239,8 +349,29 @@ public class gatchaManagement : MonoBehaviour
         }
     }
 
+    // Méthode pour définir la visibilité du bouton
+    void SetStickerCommon3CarnetVisibility(bool isVisible)
+    {
+        if (stickersCommon3Carnet != null)
+        {
+            stickersCommon3Carnet.SetActive(isVisible);
+        }
+        else
+        {
+        }
+    }
 
-
+    // Méthode pour définir la visibilité du bouton
+    void SetStickerCommon4CarnetVisibility(bool isVisible)
+    {
+        if (stickersCommon4Carnet != null)
+        {
+            stickersCommon4Carnet.SetActive(isVisible);
+        }
+        else
+        {
+        }
+    }
 
 
     // Méthode pour définir la visibilité du bouton
